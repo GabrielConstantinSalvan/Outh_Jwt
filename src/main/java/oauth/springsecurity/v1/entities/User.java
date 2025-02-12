cd97bd0e3a01b68517842450dfc6fbf04a7f7ea1
Gabriel Constantin Salvan <90073060+GabrielConstantinSalvan@users.noreply.github.com>
2025-02-12 13:31:37 -0300

    resolvendo banco de dados mysql

--- src/main/java/oauth/springsecurity/v1/entities/User.java
+++ src/main/java/oauth/springsecurity/v1/entities/User.java
@@ -1,38 +1,34 @@
-package oauth.springsecurity.v1.entities;
+package tech.buildrun.springsecurity.entities;
 
 import jakarta.persistence.*;
-import oauth.springsecurity.v1.controller.dto.LoginRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tech.buildrun.springsecurity.controller.dto.LoginRequest;
 
 import java.util.Set;
 import java.util.UUID;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-// Define a entidade User, que representa um usuário no sistema, com suas credenciais e roles (papéis).
 @Entity
-@Table(name = "tb_users") // Define o nome da tabela no banco de dados que armazena os usuários.
+@Table(name = "tb_users")
 public class User {
 
-    @Id // Define que o campo 'userId' será a chave primária da tabela.
-    @GeneratedValue(strategy = GenerationType.UUID) // Especifica que o 'userId' será gerado como um UUID único.
-    @Column(name = "user_id") // Define o nome da coluna correspondente no banco de dados.
-    private UUID userId; // Identificador único do usuário, gerado automaticamente como UUID.
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
 
-    @Column(unique = true) // Define que o campo 'username' deve ser único na tabela.
-    private String username; // Nome de usuário único no sistema.
+    @Column(unique = true)
+    private String username;
+    private String password;
 
-    private String password; // Senha do usuário (armazenada de forma segura, criptografada).
-
-    // Relacionamento Muitos-para-Muitos com a tabela de roles (papéis) do usuário.
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
-            name = "tb_users_roles", // Nome da tabela de junção entre usuários e roles.
-            joinColumns = @JoinColumn(name = "user_id"), // Define a coluna para a chave estrangeira de 'user' na tabela de junção.
-            inverseJoinColumns = @JoinColumn(name = "role_id") // Define a coluna para a chave estrangeira de 'role' na tabela de junção.
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
     )
-    private Set<Role> roles; // Conjunto de roles atribuídos ao usuário (por exemplo, "ADMIN", "BASIC").
+    private Set<Role> roles;
 
-    // Getters e Setters:
+
     public UUID getUserId() {
         return userId;
     }
@@ -65,9 +61,7 @@ public class User {
         this.roles = roles;
     }
 
-    // Método para verificar se a senha fornecida no login corresponde à senha do usuário, após criptografada.
     public boolean isLoginCorrect(LoginRequest loginRequest, PasswordEncoder passwordEncoder) {
-        // Compara a senha fornecida com a senha armazenada no banco de dados de forma criptografada.
         return passwordEncoder.matches(loginRequest.password(), this.password);
     }
 }
