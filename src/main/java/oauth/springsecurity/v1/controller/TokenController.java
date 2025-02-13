693a2280a926d06033386e822f2eab7d2176b80b
Gabriel Constantin Salvan <90073060+GabrielConstantinSalvan@users.noreply.github.com>
2025-02-13 13:08:01 -0300

    arrumando o listar

--- src/main/java/oauth/springsecurity/v1/controller/TokenController.java
+++ src/main/java/oauth/springsecurity/v1/controller/TokenController.java
@@ -22,11 +22,10 @@ import java.util.stream.Collectors;
 @RestController
 public class TokenController {
 
-    private final JwtEncoder jwtEncoder; // Responsável por gerar tokens JWT.
-    private final UserRepository userRepository; // Repositório de usuários para buscar dados no banco.
-    private BCryptPasswordEncoder passwordEncoder; // Responsável por codificar e verificar senhas.
+    private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
-    // Construtor para injetar as dependências.
     public TokenController(JwtEncoder jwtEncoder,
                            UserRepository userRepository,
                            BCryptPasswordEncoder passwordEncoder) {
@@ -35,30 +34,23 @@ public class TokenController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    // Mapeia a rota POST "/login" para autenticação do usuário.
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
 
-        // Busca o usuário no banco de dados pelo username.
         var user = userRepository.findByUsername(loginRequest.username());
 
-        // Verifica se o usuário existe e se a senha está correta.
         if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)) {
-            throw new BadCredentialsException("user or password is invalid!"); // Se inválido, lança exceção.
+            throw new BadCredentialsException("user or password is invalid!");
         }
 
-        // Obtém o instante atual e define o tempo de expiração do token (300 segundos = 5 minutos).
         var now = Instant.now();
         var expiresIn = 300L;
 
-        // Mapeia as roles do usuário para um formato adequado no JWT (prefixando com "ROLE_").
         var scopes = user.get().getRoles()
                 .stream()
                 .map(Role::getName)
                 .collect(Collectors.joining(" "));
 
-
-        // Constrói as claims (informações dentro do token JWT).
         var claims = JwtClaimsSet.builder()
                 .issuer("mybackend")
                 .subject(user.get().getUserId().toString())
@@ -67,10 +59,8 @@ public class TokenController {
                 .claim("scope", scopes)
                 .build();
 
-        // Gera o token JWT baseado nas claims definidas.
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
 
-        // Retorna o token gerado e o tempo de expiração na resposta.
         return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
     }
 }
